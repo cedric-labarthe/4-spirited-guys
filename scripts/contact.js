37aed6adc3329d14d0452eebc2759a9fe9760189
Arthur Sallé <arthursalle.pro@gmail.com>
2021-03-25 10:54:12 +0100

    finish contact & about

--- scripts/contact.js
+++ scripts/contact.js
@@ -1,8 +1,43 @@
-const popin = document.querySelector(".hidden")
-const btn = document.getElementById("btn-form")
+const popin = document.querySelector(".popin");
+const btn = document.getElementById("btn-form");
+const inputName = document.getElementById("name");
+const inputMail = document.getElementById("email");
+const inputMsg = document.getElementById("message");
 
-btn.addEventListener("click", popMsg)
+btn.addEventListener("click", popup);
+
+function popup() {
+  if (inputName.value && inputMail.value && inputMsg.value) {
+    popMsg();
+  }
+}
 
 function popMsg() {
-    popin.classList.toggle("popin")
+  popin.classList.toggle("hidden");
+}
+
+popin.addEventListener("click", (evt) => {
+  clickOnPopinContainer(evt);
+});
+
+clickOnPopinContainer = (e) => {
+  if (e.target === e.currentTarget) {
+    popin.classList.toggle("popinOffEffect");
+    popin.classList.toggle("popinOnEffect");
+    setTimeout(() => {
+      popin.classList.toggle("popinOff");
+    }, 170);
+  }
+};
+
+burger.addEventListener("click", burgerClosesPopin);
+
+function burgerClosesPopin() {
+  if (!popin.classList.contains("popinOff")) {
+    popin.classList.toggle("popinOffEffect");
+    popin.classList.toggle("popinOnEffect");
+    setTimeout(() => {
+      popin.classList.toggle("popinOff");
+    }, 190);
+  }
 }
